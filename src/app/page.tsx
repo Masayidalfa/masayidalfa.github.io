e1c934206646203a1354576abfdaacad1fa6b7bf
Masayidalfa <masayidalfarizqi@gmail.com>
2025-01-28 19:54:30 +0700

    fix stacking section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,28 +23,28 @@ export default function Home() {
       {/* BG Animation End */}
 
       {/* Navbar Start */}
-      <nav className="w-full flex justify-between items-center p-10 h-[100px] bg-transparent z-20">
-        <h1 className="text-4xl font-semibold text-white tracking-widest cursor-pointer">
+      <nav className="w-full flex justify-between items-center p-10 h-[100px] bg-transparent">
+        <h1 className="text-4xl font-semibold text-white tracking-widest cursor-pointer relative">
           <span className="text-cyan-400">MASAYID</span>ALFA
         </h1>
-        <ul className="flex space-x-6">
-          <li className="relative text-lg font-medium cursor-pointer hover:text-cyan-400">
+        <ul className="flex space-x-6 relative">
+          <li className="text-lg font-medium cursor-pointer hover:text-cyan-400">
             Home
             <div className="absolute bottom-1 left-1/2 w-0 h-[3px] bg-cyan-400 transform -translate-x-1/2 transition-all duration-300 group-hover:w-4/5"></div>
           </li>
-          <li className="relative text-lg font-medium cursor-pointer hover:text-cyan-400">
+          <li className="text-lg font-medium cursor-pointer hover:text-cyan-400">
             About
             <div className="absolute bottom-1 left-1/2 w-0 h-[3px] bg-cyan-400 transform -translate-x-1/2 transition-all duration-300 group-hover:w-4/5"></div>
           </li>
-          <li className="relative text-lg font-medium cursor-pointer hover:text-cyan-400">
+          <li className="text-lg font-medium cursor-pointer hover:text-cyan-400">
             Tools and System
             <div className="absolute bottom-1 left-1/2 w-0 h-[3px] bg-cyan-400 transform -translate-x-1/2 transition-all duration-300 group-hover:w-4/5"></div>
           </li>
-          <li className="relative text-lg font-medium cursor-pointer hover:text-cyan-400">
+          <li className="text-lg font-medium cursor-pointer hover:text-cyan-400">
             Portofolio
             <div className="absolute bottom-1 left-1/2 w-0 h-[3px] bg-cyan-400 transform -translate-x-1/2 transition-all duration-300 group-hover:w-4/5"></div>
           </li>
-          <li className="relative text-lg font-medium cursor-pointer hover:text-cyan-400">
+          <li className="text-lg font-medium cursor-pointer hover:text-cyan-400">
             Certification
             <div className="absolute bottom-1 left-1/2 w-0 h-[3px] bg-cyan-400 transform -translate-x-1/2 transition-all duration-300 group-hover:w-4/5"></div>
           </li>
@@ -55,11 +55,11 @@ export default function Home() {
       {/* Hero Section Start */}
       <section className="flex justify-center items-center h-[calc(100vh-100px)] text-center">
         <div className="text-white">
-          <h1 className="text-5xl font-bold">
+          <h1 className="text-5xl font-bold relative">
             <span className="text-cyan-400">Muhamad</span> Masayid Alfarizqi
           </h1>
-          <p className="text-lg my-4">This is your introductory text here</p>
-          <button className="px-6 py-2 bg-cyan-400 text-black font-semibold text-lg rounded-lg transition hover:bg-cyan-600">
+          <p className="text-lg my-4 relative">This is your introductory text here</p>
+          <button className="px-6 py-2 bg-cyan-400 text-black font-semibold text-lg rounded-lg transition hover:bg-cyan-600 relative">
             Contact Me
           </button>
         </div>
